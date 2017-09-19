0433186b708f9a59947b10fb3875206b40997119
Travis Bates <travbates93@gmail.com>
2017-09-19 12:11:57 -0600

    Finished with afternoon project

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,13 @@ import "./App.css";
 
 import Nav from "./Nav/Nav";
 
+import router from "../router.js"
+
 export function App( { children } ) {
 	return (
 		<div className="app">
 			<Nav />
+			{router}
 		</div>
 	);
 }
